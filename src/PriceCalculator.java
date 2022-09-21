917dae89462b60b89e02641c9e02fd2953d7d0f3
tianyuz3 <114007144+tianyuz3@users.noreply.github.com>
2022-09-21 12:27:57 -0400

    Update PriceCalculator.java

--- src/PriceCalculator.java
+++ src/PriceCalculator.java
@@ -1,6 +1,24 @@
 import java.util.Scanner;
 public class PriceCalculator {
     public static void main(String[] args) {
+        Scanner t=new Scanner(System.in);
+        Scanner p=new Scanner(System.in);
+        Scanner n=new Scanner(System.in);
+        System.out.print("Enter your total bill without tax:");
+        Double totalBill=t.nextDouble();
+        System.out.print("Enter the tip percentage:");
+        Double tipPercentage=p.nextDouble();
+        System.out.print("Number of people:");
+        int numberOfPeople=n.nextInt();
+
+        Double tipPerPerson= totalBill*tipPercentage/numberOfPeople;
+        Double totalPerPerson= totalBill/numberOfPeople+tipPerPerson;
+        Double tipAmount=totalBill*tipPercentage;
+        Double billIncludingTips=totalBill*(1+tipPercentage);
+
+        System.out.println("Tip amount:" + "$" + String.format("%.2f",tipAmount) );
+        System.out.println("Bill including tips:"+ String.format("%.2f",billIncludingTips));
+        System.out.println("")
 
     }
 }
